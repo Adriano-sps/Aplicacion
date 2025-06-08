1d69d794fbc50e418bbc07accc2ccc86a94f6c1c
LuisMrio <l.badillofernandez@gmail.com>
2025-06-08 09:52:43 -0500

    pausa
    iré a desayunar

---
+++ mi_app/lib/features/home/presentation/views/initial.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Initialview extends StatelessWidget {
+  const Initialview({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/ballon.jpg'), fit: BoxFit.cover)
+        ),
+        
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+           
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 800.0,),
+                Text('Bienvenidos pulse entrar para ver los nominados a bolon de oro 2025', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15.0),),
+                SizedBox(height: 50.0,),
+                Row(
+                 children: [
+                  ElevatedButton(
+                  
+                    style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 255, 221, 2),foregroundColor: Colors.black, fixedSize: Size(320, 50)),
+                 
+                    onPressed: () {
+                    
+                    },
+                    
+                    child: const Text('Entrar', style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),),
+    
+                  ),
+    
+                 ],
+    
+                )
+    
+              
+              ],
+              
+            )
+          ],
+        ),
+    
+      
+    
+    
+        ),
+    
+    
+    
+    
+      ),
+    
+    );
+  }
+}
