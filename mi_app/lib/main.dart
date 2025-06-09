30e2431d44a8005ca7b31a31452d5a8132f346a9
Adriano <adrianosegudop@gmail.com>
2025-06-09 11:56:50 -0500

    Bloc terminado

--- mi_app/lib/main.dart
+++ mi_app/lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mi_app/features/home/presentation/bloc/home_bloc.dart';
 
 import 'features/home/presentation/views/failure.dart';
 import 'features/home/presentation/views/initial.dart';
 import 'features/home/presentation/views/loading.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
   @override
-  Widget build(BuildContext context) {
-    return Failureview();
-    
+  Widget build(BuildContext context) { 
+    return BlocProvider(
+      create: (context) => HomeBloc(),
+      child: MaterialApp(
+          title: 'My APP',
+          home: BlocBuilder<HomeBloc, HomeState>(
+            builder: (context, state) {
+              if (state is HomeLoadSuccess){
+                return Success();
+              }else if (state is HomeLoadInProgress){
+                return Loading();
+              }else if (state is HomeLoadFailure){
+                return Failure();
+              }
+              return Initial();
+            },
+          )),
+    );
   }
 }
-
-
-
 
